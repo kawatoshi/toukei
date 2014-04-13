7f1fe714626c4c2ca7031606558e397265f3eed4
Kawakita Toshikazu <kawakita@fujibil.co.jp>
2014-04-13 16:32:46 +0900

    パラメータの格納方法及び表示方法を変更

--- syuukei/cgi/index.rb
+++ syuukei/cgi/index.rb
@@ -5,6 +5,23 @@ require 'erb'
 
 data = YAML.load_file('/var/data/buppan/config/config.yaml')
 erb = ERB.new(File.read('../rhtml/index.rhtml'), nil, '-')
+
+def get_param(content, m_list_one)
+	ary = []
+	content["params"].each{|key|
+		if m_list_one["params"][key] then
+			ary << "#{key}=#{m_list_one["params"][key]}"
+		end
+	}
+	"#{content["command"]}#{ary.join("&")}"
+end
 title = data["main"]["title"]
 puts "Content-Type:text/html; charset=utf-8\n\n"
 puts erb.result(binding)
+#p get_param(data["main"]["contents"][2], data["m_list"][0])
+#data["main"]["contents"].each{|con|
+#	puts con["name"]
+#	data["m_list"].each{|list_one|
+#		puts get_param(con, list_one)
+#	}
+#}
