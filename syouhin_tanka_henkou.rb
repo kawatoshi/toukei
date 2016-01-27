7e6a66a673438033cd7805ae99dcbdcc059c4791
kawakita <kawakita@fujibil.co.jp>
2016-01-27 15:19:57 +0900

    F12キーがフォルダオープンになってしまう為の変更

--- syouhin_tanka_henkou.rb
+++ syouhin_tanka_henkou.rb
@@ -1,27 +1,28 @@
 #!/usr/bin/ruby
 # -*- coding: utf-8 -*-
-require './inputkey'
-include Inputkey
 
 open(ARGV[0].chomp, 'r') do |io|
-	s_name = Inputkey::Inputkey.new("")
 	while line = io.gets
 		ary = line.split(/\t/)
 		s_code = ary[0]
 		u_tanka = ary[2]
 
-		s_name.key s_code
-		s_name.enter 2
-		s_name.key_wait 800
-		s_name.enter 8
-		s_name.key u_tanka
-		s_name.enter
-		s_name.key_wait 400
-
-		puts "F12"
-		s_name.key_wait(700)
-		s_name.enter
-		s_name.key_wait(900)
+		puts "key #{s_code}"
+		puts "enter"
+		puts "enter"
+		puts "800"
+		puts "<r 8>"
+		puts "enter"
+		puts "</r>"
+		puts "key #{u_tanka}"
+		puts "enter"
+		puts "400"
+		puts "LMouse Down (1057,1231)"
+		puts "LMouse Up (1057,1231)"
+		puts "1200"
+		puts "enter"
+		puts "900"
+		puts ""
 	end
 end
 
